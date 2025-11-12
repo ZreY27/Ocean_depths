55e1c3d602e79556e99914627dc8d5ccc9ec5042
antoine <a.reelitin@gmail.com>
2025-11-12 12:58:09 +0100

    système combat full opérationnel (sauf inventaire)

--- code/include/affichage.h
+++ code/include/affichage.h
@@ -13,6 +13,7 @@ typedef enum{
 
 void afficherCombat(const CreatureMarine * creature, const Plongeur * joueur, 
     const char* message, const char* menu, EtatAffichage etat_affichage);
+void afficherTexteAnime(const char* texte, int delai_us);
 void afficherBarrePV(int pv_actuels, int pv_max, int longueur);
 void afficheInventaire(Inventaire inventaire);
 void afficherCarte(Carte carte);
